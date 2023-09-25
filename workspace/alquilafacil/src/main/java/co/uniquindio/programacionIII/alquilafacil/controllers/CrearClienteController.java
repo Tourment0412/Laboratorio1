c27ad39c8e5c91f215413270260f4db4a7fb6963
Tourment0412 <94409923+Tourment0412@users.noreply.github.com>
2023-09-25 15:56:23 -0500

    update controller

--- workspace/alquilafacil/src/main/java/co/uniquindio/programacionIII/alquilafacil/controllers/CrearClienteController.java
+++ workspace/alquilafacil/src/main/java/co/uniquindio/programacionIII/alquilafacil/controllers/CrearClienteController.java
@@ -1,5 +1,40 @@
 package co.uniquindio.programacionIII.alquilafacil.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class CrearClienteController {
+
+    @FXML
+    private Button btnRegistrar;
+
+    @FXML
+    private Label lblTitle;
+
+    @FXML
+    private TextField txtCedula;
+
+    @FXML
+    private TextField txtCiudad;
+
+    @FXML
+    private TextField txtDireccion;
+
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    private TextField txtNombre;
+
+    @FXML
+    private TextField txtNumero;
+
+    @FXML
+    void registrarEvent(ActionEvent event) {
+
+    }
 
 }
