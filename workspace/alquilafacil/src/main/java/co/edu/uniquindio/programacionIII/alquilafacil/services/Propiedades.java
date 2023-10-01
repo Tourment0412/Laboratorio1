a9f8dc330452bebe32738c07f8d83d8c42382b82
Juan Manuel Amador Roa <115110279+Corem05@users.noreply.github.com>
2023-09-30 22:54:05 -0500

    Add property handler class

---
+++ workspace/alquilafacil/src/main/java/co/edu/uniquindio/programacionIII/alquilafacil/services/Propiedades.java
@@ -0,0 +1,34 @@
+package co.edu.uniquindio.programacionIII.alquilafacil.services;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.function.Consumer;
+
+import javafx.beans.property.SimpleObjectProperty;
+
+public class Propiedades {
+	private static final String RUTA = "/co/edu/uniquindio/programacionIII/alquilafacil/properties/textos";
+
+	private final SimpleObjectProperty<ResourceBundle> bundleProperty = new SimpleObjectProperty<>();
+
+	private static Propiedades instance;
+
+	private Propiedades() {
+		bundleProperty.setValue(ResourceBundle.getBundle(RUTA));
+	}
+
+	public void setLanguage(Locale locale) {
+		bundleProperty.setValue(ResourceBundle.getBundle(RUTA, locale));
+	}
+
+	public void addListener(Consumer<ResourceBundle> listener) {
+		listener.accept(bundleProperty.getValue());
+		bundleProperty.addListener((observable, oldValue, newValue) -> listener.accept(newValue));
+	}
+
+	public static Propiedades getInstance() {
+		if (instance == null)
+			instance = new Propiedades();
+		return instance;
+	}
+}
