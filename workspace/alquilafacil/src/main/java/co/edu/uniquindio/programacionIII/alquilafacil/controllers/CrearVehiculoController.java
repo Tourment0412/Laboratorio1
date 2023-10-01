46e79adf1ccc1b81c54937a9d73cbbf5c9b74d37
Tourment0412 <santiquinterouribe0412@gmail.com>
2023-10-01 14:53:09 -0500

    Added crearVehiculo fxml, added some css styles and changes in other css styles, added controller

---
+++ workspace/alquilafacil/src/main/java/co/edu/uniquindio/programacionIII/alquilafacil/controllers/CrearVehiculoController.java
@@ -0,0 +1,90 @@
+package co.edu.uniquindio.programacionIII.alquilafacil.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TextField;
+
+public class CrearVehiculoController {
+
+    @FXML
+    private Button btnCrearVehiculo;
+
+    @FXML
+    private Button btnImagen;
+
+    @FXML
+    private ComboBox<?> cmbTransmision;
+
+    @FXML
+    private DatePicker dateFecha;
+
+    @FXML
+    private Label lblFecha;
+
+    @FXML
+    private Label lblImagen;
+
+    @FXML
+    private Label lblKilometraje;
+
+    @FXML
+    private Label lblMarca;
+
+    @FXML
+    private Label lblModelo;
+
+    @FXML
+    private Label lblNombre;
+
+    @FXML
+    private Label lblPlaca;
+
+    @FXML
+    private Label lblPrecioDia;
+
+    @FXML
+    private Label lblSillas;
+
+    @FXML
+    private Label lblTitle;
+
+    @FXML
+    private Label lblTransmision;
+
+    @FXML
+    private Spinner<?> spnKilometraje;
+
+    @FXML
+    private Spinner<?> spnModelo;
+
+    @FXML
+    private Spinner<?> spnSilas;
+
+    @FXML
+    private TextField tctPrecioDia;
+
+    @FXML
+    private TextField txtMarca;
+
+    @FXML
+    private TextField txtNombre;
+
+    @FXML
+    private TextField txtPlaca;
+
+    @FXML
+    void crearVehiculoEvent(ActionEvent event) {
+
+    }
+
+    @FXML
+    void seleccionarImagenEvent(ActionEvent event) {
+
+    }
+
+}
