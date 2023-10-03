e850082f388f5a009a7795ef51a5c500cb07ed3a
Juan Manuel Amador Roa <115110279+Corem05@users.noreply.github.com>
2023-10-02 23:45:08 -0500

    Add title and image for application

--- workspace/alquilafacil/src/main/java/co/edu/uniquindio/programacionIII/alquilafacil/aplication/App.java
+++ workspace/alquilafacil/src/main/java/co/edu/uniquindio/programacionIII/alquilafacil/aplication/App.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import co.edu.uniquindio.programacionIII.alquilafacil.viewcontrollers.MainViewController;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class App extends Application {
@@ -14,7 +15,8 @@ public class App extends Application {
 		stage.setScene(new Scene(MainViewController.getInstance().loadMainMenu(), 640, 480));
 		stage.setMinHeight(600);
 		stage.setMinWidth(1000);
-		
+		stage.setTitle("Alquila Facil");
+		stage.getIcons().add(new Image("/co/edu/uniquindio/programacionIII/alquilafacil/sources/logo.png"));
 		stage.show();
 	}
 
