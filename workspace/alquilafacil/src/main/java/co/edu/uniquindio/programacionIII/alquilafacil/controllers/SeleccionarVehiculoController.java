db54f5c117abf01664e7f3ae7156bfb3a1b66d31
Tourment0412 <94409923+Tourment0412@users.noreply.github.com>
2023-10-03 21:46:22 -0500

    Added traductions

--- workspace/alquilafacil/src/main/java/co/edu/uniquindio/programacionIII/alquilafacil/controllers/SeleccionarVehiculoController.java
+++ workspace/alquilafacil/src/main/java/co/edu/uniquindio/programacionIII/alquilafacil/controllers/SeleccionarVehiculoController.java
@@ -1,17 +1,22 @@
 package co.edu.uniquindio.programacionIII.alquilafacil.controllers;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import co.edu.uniquindio.programacionIII.alquilafacil.model.Vehiculo;
+import co.edu.uniquindio.programacionIII.alquilafacil.utils.Propiedades;
 import co.edu.uniquindio.programacionIII.alquilafacil.utils.Vista;
 import co.edu.uniquindio.programacionIII.alquilafacil.viewcontrollers.MainViewController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 
-public class SeleccionarVehiculoController {
+public class SeleccionarVehiculoController implements Initializable {
 
     @FXML
     private Button btnSiguiente;
@@ -72,6 +77,26 @@ public class SeleccionarVehiculoController {
 
 	private void volverAc1tion() {
 		MainViewController.getInstance().cambiarVista(Vista.RENT_INI);
+		
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		Propiedades.getInstance().addListener(bundle->{
+			lblTitle.setText(bundle.getString("SeleccionarVehiculo.lblTitle"));
+			lblPlaca.setText(bundle.getString("SeleccionarVehiculo.lblPlaca"));
+			colPlaca.setText(bundle.getString("SeleccionarVehiculo.colPlaca"));
+			colNombre.setText(bundle.getString("SeleccionarVehiculo.colNombre"));
+			colMarca.setText(bundle.getString("SeleccionarVehiculo.colMarca"));
+			colModelo.setText(bundle.getString("SeleccionarVehiculo.colModelo"));
+			colKilometraje.setText(bundle.getString("SeleccionarVehiculo.colKilometraje"));
+			colPrecioDia.setText(bundle.getString("SeleccionarVehiculo.colPrecioDia"));
+			colTransmision.setText(bundle.getString("SeleccionarVehiculo.colTransmision"));
+			colSillas.setText(bundle.getString("SeleccionarVehiculo.colSillas"));
+			btnSiguiente.setText(bundle.getString("SeleccionarVehiculo.btnSiguiente"));
+			btnVolver.setText(bundle.getString("SeleccionarVehiculo.btnVolver"));
+			tblVehiculos.setPlaceholder(new Label(bundle.getString("TablaSinContenido")));
+		});
 		
 	}
     
