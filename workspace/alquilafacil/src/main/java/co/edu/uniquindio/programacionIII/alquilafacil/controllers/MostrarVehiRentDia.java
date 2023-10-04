5aa378c5c38db2a0baaacd37e0fdd3831b0ef402
Tourment0412 <94409923+Tourment0412@users.noreply.github.com>
2023-10-03 22:22:01 -0500

    Traductions finished

--- workspace/alquilafacil/src/main/java/co/edu/uniquindio/programacionIII/alquilafacil/controllers/MostrarVehiRentDia.java
+++ workspace/alquilafacil/src/main/java/co/edu/uniquindio/programacionIII/alquilafacil/controllers/MostrarVehiRentDia.java
@@ -1,17 +1,22 @@
 package co.edu.uniquindio.programacionIII.alquilafacil.controllers;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import co.edu.uniquindio.programacionIII.alquilafacil.model.Vehiculo;
+import co.edu.uniquindio.programacionIII.alquilafacil.utils.Propiedades;
 import co.edu.uniquindio.programacionIII.alquilafacil.utils.Vista;
 import co.edu.uniquindio.programacionIII.alquilafacil.viewcontrollers.MainViewController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
-public class MostrarVehiRentDia {
+public class MostrarVehiRentDia implements Initializable {
 
     @FXML
     private Button btnFiltrar;
@@ -56,4 +61,20 @@ public class MostrarVehiRentDia {
     private void volverAction() {
     	MainViewController.getInstance().cambiarVista(Vista.FUNCTEXT);
     }
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		Propiedades.getInstance().addListener(bundle->{
+			lblTitle.setText(bundle.getString("MostrarVehiRentDia.lblTitle"));
+			lblFecha.setText(bundle.getString("MostrarVehiRentDia.lblFecha"));
+			btnFiltrar.setText(bundle.getString("MostrarVehiRentDia.btnFiltrar"));
+			colPlaca.setText(bundle.getString("MostrarVehiRentDia.colPlaca"));
+			colNombre.setText(bundle.getString("MostrarVehiRentDia.colNombre"));
+			colMarca.setText(bundle.getString("MostrarVehiRentDia.colMarca"));
+			colModelo.setText(bundle.getString("MostrarVehiRentDia.colModelo"));
+			btnVolver.setText(bundle.getString("MostrarVehiRentDia.btnVolver"));
+			tblVehiculos.setPlaceholder(new Label(bundle.getString("TablaSinContenido")) );
+		});
+		
+	}
 }
