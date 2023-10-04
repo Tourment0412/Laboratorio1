364fbd3fb2dc753b2005fea887ba48266d3f0c64
Juan Manuel Amador Roa <115110279+Corem05@users.noreply.github.com>
2023-10-04 00:25:43 -0500

    Organized imports

--- workspace/alquilafacil/src/main/java/co/edu/uniquindio/programacionIII/alquilafacil/controllers/CrearVehiculoController.java
+++ workspace/alquilafacil/src/main/java/co/edu/uniquindio/programacionIII/alquilafacil/controllers/CrearVehiculoController.java
@@ -20,7 +20,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
-import javafx.scene.control.Spinner;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 
