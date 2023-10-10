076804bb44c850fdd25db01d671a57d0040d32ac
Tourment0412 <94409923+Tourment0412@users.noreply.github.com>
2023-10-10 12:32:40 -0500

    Some changes

--- workspace/alquilafacil/src/main/java/co/edu/uniquindio/programacionIII/alquilafacil/controllers/MostrarVehiRentDia.java
+++ workspace/alquilafacil/src/main/java/co/edu/uniquindio/programacionIII/alquilafacil/controllers/MostrarVehiRentDia.java
@@ -1,12 +1,18 @@
 package co.edu.uniquindio.programacionIII.alquilafacil.controllers;
 
 import java.net.URL;
+import java.text.DecimalFormat;
 import java.util.ResourceBundle;
 
+import co.edu.uniquindio.programacionIII.alquilafacil.exceptions.CampoInvalidoException;
+import co.edu.uniquindio.programacionIII.alquilafacil.exceptions.ListaVaciaException;
 import co.edu.uniquindio.programacionIII.alquilafacil.model.Vehiculo;
 import co.edu.uniquindio.programacionIII.alquilafacil.utils.Propiedades;
+import co.edu.uniquindio.programacionIII.alquilafacil.utils.Utils;
 import co.edu.uniquindio.programacionIII.alquilafacil.utils.Vista;
 import co.edu.uniquindio.programacionIII.alquilafacil.viewcontrollers.MainViewController;
+import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -15,6 +21,7 @@ import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.Alert.AlertType;
 
 public class MostrarVehiRentDia implements Initializable {
 
@@ -28,7 +35,7 @@ public class MostrarVehiRentDia implements Initializable {
     private TableColumn<Vehiculo, String> colMarca;
 
     @FXML
-    private TableColumn<Vehiculo, Integer> colModelo;
+    private TableColumn<Vehiculo, String> colModelo;
 
     @FXML
     private TableColumn<Vehiculo, String> colNombre;
@@ -47,23 +54,17 @@ public class MostrarVehiRentDia implements Initializable {
 
     @FXML
     private TableView<Vehiculo> tblVehiculos;
-
-    @FXML
-    void filtrarEvent(ActionEvent event) {
-
-    }
     
-    @FXML
-    void volverEvent(ActionEvent event) {
-    	volverAction();
-    }
-
-    private void volverAction() {
-    	MainViewController.getInstance().cambiarVista(Vista.FUNCTEXT);
-    }
-
-	@Override
+    
+    @Override
 	public void initialize(URL location, ResourceBundle resources) {
+		
+    	DecimalFormat format = new DecimalFormat("#");
+		colMarca.setCellValueFactory(cell -> new ReadOnlyStringWrapper(cell.getValue().getMarca()));
+		colModelo.setCellValueFactory(cell -> new ReadOnlyStringWrapper(format.format(cell.getValue().getModelo())));
+		colNombre.setCellValueFactory(cell -> new ReadOnlyStringWrapper(cell.getValue().getNombre()));
+		colPlaca.setCellValueFactory(cell -> new ReadOnlyStringWrapper(cell.getValue().getPlaca()));
+		
 		Propiedades.getInstance().addListener(bundle->{
 			lblTitle.setText(bundle.getString("MostrarVehiRentDia.lblTitle"));
 			lblFecha.setText(bundle.getString("MostrarVehiRentDia.lblFecha"));
@@ -77,4 +78,30 @@ public class MostrarVehiRentDia implements Initializable {
 		});
 		
 	}
+
+    @FXML
+    void filtrarEvent(ActionEvent event) {
+    	
+    	try {
+			tblVehiculos.setItems(FXCollections.observableArrayList(ModelFactoryController.getInstance().
+					listarVehiculosFecha(dtpFecha.getValue())));
+					;
+			tblVehiculos.refresh();
+			
+		} catch (ListaVaciaException | CampoInvalidoException e) {
+			Utils.mostrarAlerta("Advertencia", e.getMessage(), AlertType.WARNING);
+		}
+
+    }
+    
+    @FXML
+    void volverEvent(ActionEvent event) {
+    	volverAction();
+    }
+
+    private void volverAction() {
+    	MainViewController.getInstance().cambiarVista(Vista.FUNCTEXT);
+    }
+
+	
 }
